1dce3eab104a0714d7040c05e9c3f02f8abef7ab
sengarsakshi2005-coder <sengarsakshi2005@gmail.com>
2026-01-29 12:05:53 +0530

    [Sakshi]

---
+++ Arrays_program/array_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class array  {
+	public static void main(String[]args) {
+		int arr [] = new int[5];
+		arr [0] = 99;
+		arr [1] = 67;
+		arr [2] = 108;
+		arr [3] = 45;
+		arr [4] = 70;
+		System.out.println("print array with string :- ");
+		System.out.println( Arrays.toString(arr));
+		
+		System.out.println("Lenght of an array :- " );
+		System.out.println(arr.length );
+		
+	}
+}
